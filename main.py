ff48977c4c32fa641862148eb0195e54fc4dcff8
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-11 16:41:10 -0600

    Update main.py
    cleaned up some of the functionality using functions

--- main.py
+++ main.py
@@ -3,40 +3,63 @@ from art import logo
 
 print(logo)
 
-turns_available = 0
-game_over = 0
 
-print("Welcome to the number guessing game!")
-print("I'm thinking of a number between 1 and 100.")
+def set_difficulty():
+    """
+    Function to set the difficulty of the game.
+    :return int:
+    """
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard: ").lower()
+    if difficulty == 'hard':
+        return 5
+    else:
+        return 10
 
-num_to_guess = random.randint(1, 100)
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard: ").lower()
-if difficulty == 'hard':
-    turns_available = 5
-else:
-    turns_available = 10
-
-while game_over != 1:
-    print(f"You have {turns_available} attempts remaining to guess the number.")
-    player_guess = int(input("Enter a guess: "))
-
-    if player_guess < num_to_guess:
+def compare(p_guess, num):
+    """
+    Compares the player's guess to the randomly chosen number.
+    :param p_guess:
+    :param num:
+    :return bool:
+    """
+    if p_guess < num:
         print("Too low.")
-    elif player_guess > num_to_guess:
+        return False
+    elif p_guess > num:
         print("Too high.")
-    elif player_guess == num_to_guess:
-        game_over = 1
-        print(f"You win! The answer was: {num_to_guess}")
-
-    turns_available -= 1
-    if turns_available == 0:
-        game_over = 1
-
-if turns_available == 0 and game_over == 1:
-    print("You have run out of guesses, you lose.")
+        return False
+    elif p_guess == num:
+        print(f"You win! The answer was: {num}")
+        return True
 
 
+def welcome_message():
+    print("Welcome to the number guessing game!")
+    print("I'm thinking of a number between 1 and 100.")
 
 
+def game():
+    turns_available = set_difficulty()
+    game_over = False
 
+    welcome_message()
+
+    num_to_guess = random.randint(1, 100)
+
+    while game_over != 1:
+        print(f"You have {turns_available} attempts remaining to guess the number.")
+        player_guess = int(input("Enter a guess: "))
+
+        game_over = compare(p_guess=player_guess, num=num_to_guess)
+
+        turns_available -= 1
+
+        if turns_available == 0:
+            game_over = True
+
+    if turns_available == 0 and game_over:
+        print("You have run out of guesses, you lose.")
+
+
+game()
